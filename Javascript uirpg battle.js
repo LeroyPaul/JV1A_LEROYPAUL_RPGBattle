b9af94aa0017806ca63b16c89b192f58357754b7
LeroyPaul <paulo.leroy2@gmail.com>
2021-12-01 11:27:12 +0100

    images

---
+++ Javascript uirpg battle.js
@@ -0,0 +1,33 @@
+var pvBernard = document.getElementById("pvbernardlhermite")
+var attaquebernard = document.getElementById("attaquebernard")
+var pvHiboux = document.getElementById("pvhibouxrose")
+pvHiboux.innerHTML=200
+pvBernard.innerHTML=100
+attaquebernard.onclick= function (){
+    pvHiboux.innerHTML -= 4 
+}
+
+
+// partie principale du code
+victory=false
+
+while (victory == false){
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
